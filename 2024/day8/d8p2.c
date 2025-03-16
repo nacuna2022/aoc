3d1860cb74aa67c6ca7c20bbd24ef6e5d7684091
nacuna <niel.acuna@gmail.com>
2025-03-16 17:24:07 +0800

    add day 8 part 2

--- 2024/day8/d8p2.c
+++ 2024/day8/d8p2.c
@@ -16,6 +16,7 @@ struct tile_map {
 	char *buffer;
 	size_t size;
 	size_t line_size;
+	struct antenna *antinode_list;
 	struct antenna *antenna_lut[CHAR_MAX];
 };
 
@@ -52,6 +53,7 @@ static void init_tile_map(struct tile_map *map, char *pathname)
 	fclose(input);
 	map->buffer = buffer;
 	map->size = map_size;
+	map->antinode_list = NULL;
 	for (i = 0; i < CHAR_MAX; i++) {
 		map->antenna_lut[i] = NULL;
 	}
@@ -134,55 +136,6 @@ static inline void get_coord(struct tile_map *map, uintptr_t loc,
 	return;
 }
 
-
-/* always install antinode with reference to ant1 */
-static bool install_antinode(struct tile_map *map, struct antenna *ant1, 
-		struct antenna *ant2, intptr_t distance)
-{
-	char *new_location;
-	int delta1;
-	int delta2;
-
-	/* distance cannot be zero! */
-	assert(distance != 0);
-	new_location = (char *)((intptr_t)ant1->location + distance);
-
-	/* check if we are stepping off the starting and ending points 
-	 * of the map. */
-	if (new_location < map->buffer)
-		return false;
-	if (new_location > ((map->buffer + map->size)-1))
-		return false;
-
-	/* if we reach here, we are still within the bounds of the start/end of 
-	 * the map. but things are more nuanced than that. so we do more 
-	 * checks. */
-	struct coord an1_coord;
-	struct coord an2_coord;
-	struct coord antinode_coord;
-
-	get_coord(map, (uintptr_t)ant1->location, &an1_coord);
-	get_coord(map, (uintptr_t)ant2->location, &an2_coord);
-	get_coord(map, (uintptr_t)new_location, &antinode_coord);
-
-	delta1 = abs(an1_coord.x - an2_coord.x);
-	delta2 = abs(an1_coord.x - antinode_coord.x);
-	if (delta1 != delta2)
-		return false;
-
-	delta1 = abs(an1_coord.y - an2_coord.y);
-	delta2 = abs(an1_coord.y - antinode_coord.y);
-	if (delta1 != delta2)
-		return false;
-
-	/* install the antinode on this tile - overwriting the antenna. */
-	if (*new_location == '#')
-		return false;
-	*new_location = '#';
-
-	return true;
-}
-
 static void print_tile_map(struct tile_map *map)
 {
 	char *p = map->buffer;
@@ -197,6 +150,93 @@ static void print_tile_map(struct tile_map *map)
 	return;
 }
 
+static void alloc_antinode(struct antenna **antpp, uintptr_t location,
+		int frequency)
+{
+	struct antenna *ant;
+	ant = malloc(sizeof *ant);
+	assert(ant != NULL);
+	ant->location = location;
+	ant->frequency = frequency;
+	ant->next = NULL;
+	*antpp = ant;
+	return;
+}
+
+static bool new_antinode_at_location(struct tile_map *map, uintptr_t location, 
+		int frequency)
+{
+	struct antenna **antinode;
+
+	/* check if we already have an antinode at location */
+	for (antinode = &map->antinode_list; *antinode != NULL; antinode = &(*antinode)->next) {
+		struct antenna *ant = *antinode;
+		if (ant->location == location) {
+			return false;
+		}
+	}
+
+	/* if we reach here, this is a unique node */
+	alloc_antinode(antinode, location, frequency);
+	
+	return true;
+}
+
+/* always install antinode with reference to ant1 */
+static int install_antinode(struct tile_map *map, struct antenna *ant1, 
+		struct antenna *ant2, intptr_t distance, int freq)
+{
+	char *new_location;
+	int delta1;
+	int delta2;
+	int count = 0;
+
+	/* distance cannot be zero! */
+	assert(distance != 0);
+	new_location = (char *)((intptr_t)ant1->location + distance);
+
+	/* check if we are stepping off the starting and ending points 
+	 * of the map. */
+	if (new_location < map->buffer)
+		return 0;
+	if (new_location > ((map->buffer + map->size)-1))
+		return 0;
+
+	/* if we reach here, we are still within the bounds of the start/end of 
+	 * the map. but things are more nuanced than that. so we do more 
+	 * checks. */
+	struct coord an1_coord;
+	struct coord an2_coord;
+	struct coord antinode_coord;
+
+	get_coord(map, ant1->location, &an1_coord);
+	get_coord(map, ant2->location, &an2_coord);
+	get_coord(map, (uintptr_t)new_location, &antinode_coord);
+
+	delta1 = abs(an1_coord.x - an2_coord.x);
+	delta2 = abs(an1_coord.x - antinode_coord.x);
+	if (delta1 != delta2)
+		return 0;
+
+	delta1 = abs(an1_coord.y - an2_coord.y);
+	delta2 = abs(an1_coord.y - antinode_coord.y);
+	if (delta1 != delta2)
+		return 0;
+
+	struct antenna ant_antinode = {
+		.location = (uintptr_t)new_location,
+	};
+
+	if (new_antinode_at_location(map, (uintptr_t)new_location, freq) == true) {
+		count += 1;
+	}
+
+	if (new_antinode_at_location(map, ant2->location, freq) == true) {
+		count += 1;
+	}
+
+	return count + install_antinode(map, &ant_antinode, ant1, distance, freq);
+}
 
 #define for_each_antenna(ant, list) \
 	for ((ant) = (list); (ant) != NULL; (ant) = (ant)->next) 
@@ -204,16 +244,19 @@ static void print_tile_map(struct tile_map *map)
 static int discover_by_antenna_list(struct tile_map *map,
 		struct antenna *ant_list)
 {
-	int antinode_count = 0;
 	struct antenna *ant1;
 	struct antenna *ant2;
 	intptr_t distance;
+	int count = 0;
 
 	for_each_antenna(ant1, ant_list) {
 		for_each_antenna(ant2, ant_list) {
-			bool install_ok;
 			if (ant1 == ant2)
 				continue;
+		
+			if (new_antinode_at_location(map, (uintptr_t)ant1->location, ant1->frequency) == true) {
+				count += 1;
+			}
 
 			/* if distance is negative: ant1 comes before ant2.
 			 * we need to install antinode _before_ ant1.
@@ -222,29 +265,31 @@ static int discover_by_antenna_list(struct tile_map *map,
 			 * we need to install antinode _after_ ant1.
 			 */
 			distance = (intptr_t)(ant1->location) - (intptr_t)(ant2->location);
-			install_ok = install_antinode(map, ant1, ant2,
-					distance);
-			antinode_count += (install_ok == true ? 1 : 0);
+			count += install_antinode(map, ant1, ant2, distance, ant1->frequency);
 		}
 	}
 
-	return antinode_count;
+	return count;
 }
 
-static void discover_all_antinodes(struct tile_map *map)
+static int discover_all_antinodes(struct tile_map *map)
 {
 	size_t i;
-	int total = 0; 
+	int count = 0;
 	for (i = 0; i < CHAR_MAX; i++) {
 		struct antenna *ant_list = map->antenna_lut[i];
 		if (ant_list == NULL) {
 			continue;
 		}
-		total += discover_by_antenna_list(map,
-				ant_list);
+
+		/* skip single antennas */
+		if (ant_list->next == NULL) {
+			continue;
+		}
+
+		count += discover_by_antenna_list(map, ant_list);
 	} 
-	printf("unique antinodes: %d\n", total);
-	return; 
+	return count; 
 }
 
 void free_antenna_list(struct antenna *list)
@@ -262,9 +307,12 @@ void free_antenna_list(struct antenna *list)
 static void free_tile_map(struct tile_map *map)
 {
 	size_t i;
+	struct antenna *ant;
+	struct antenna *tmp;
 	for (i = 0; i < CHAR_MAX; i++) {
 		free_antenna_list(map->antenna_lut[i]);
 	}
+	free_antenna_list(map->antinode_list);
 	free(map->buffer);
 	free(map);
 	return;
@@ -272,10 +320,12 @@ static void free_tile_map(struct tile_map *map)
 
 int main(void)
 {
+	int antinode_count;
 	struct tile_map *antenna_field;
 	antenna_field = new_tile_map_from_input("input");
 	scan_antenna_field(antenna_field);
-	discover_all_antinodes(antenna_field);
+	antinode_count = discover_all_antinodes(antenna_field);
+	printf("unique antinode: %d\n", antinode_count);
 	free_tile_map(antenna_field);
 	return 0;
 }
